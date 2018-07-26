c855dd40db5bf290120c6149e384ca78cc027e60
Ruben Taelman <ruben.taelman@ugent.be>
2018-07-26 10:56:36 +0200

    Remove all manual n3 require's

--- packages/actor-init-rdf-parse/test/ActorInitRdfParse-test.ts
+++ packages/actor-init-rdf-parse/test/ActorInitRdfParse-test.ts
@@ -7,9 +7,6 @@ import {ActorInitRdfParse} from "../lib/ActorInitRdfParse";
 const stringToStream = require('streamify-string');
 const arrayifyStream = require('arrayify-stream');
 
-const n3 = require('@comunica/actor-rdf-parse-n3/node_modules/n3/N3.js');
-Object.keys(n3).forEach((submodule) => n3[submodule] = n3[submodule]);
-
 describe('ActorInitRdfParse', () => {
   let bus;
   let busInit;
